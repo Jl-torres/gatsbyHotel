6e509cd446ebce15205feb56b626753fb8194bdc
Josmar <ltjosmar1997@gmail.com>
2021-02-08 17:29:34 -0400

    Updates

---
+++ component---src-pages-404-js-bb2f9c2c5554219a886e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(t,n,e){"use strict";e.r(n);e("q1tI");var u=e("Bl7J"),o=e("AeFk");n.default=function(){return Object(o.b)(u.a,null,Object(o.b)("h1",null,"404: Not Found"),Object(o.b)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-bb2f9c2c5554219a886e.js.map
